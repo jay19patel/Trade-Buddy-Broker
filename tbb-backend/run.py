40d49b71ca05f758b43e078cd7fba55bc410277c
Jay Patel <developerjay19@gmail.com>
2024-09-21 20:34:18 +0530

    add req.txt file

--- tbb-backend/run.py
+++ tbb-backend/run.py
@@ -1,5 +1,7 @@
 from app.main import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    import uvicorn
+    uvicorn.run("run:app", host="0.0.0.0", port=8000, reload=True)
 
