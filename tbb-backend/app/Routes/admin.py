140209a97b23e60b7befea9fae105beeff90eec5
developerjay19 <developer.jay19@gmail.com>
2024-11-04 11:37:44 +0530

    Add Datetime setting and also update accounts dashbord count and more again

--- tbb-backend/app/Routes/admin.py
+++ tbb-backend/app/Routes/admin.py
@@ -141,8 +141,8 @@ async def get_accounts(
             COALESCE(SUM(CASE WHEN t.transaction_type = 'WITHDRAW' THEN t.transaction_amount ELSE 0 END), 0) AS total_amount,
             
             (SELECT COUNT(*) FROM positions p WHERE p.account_id = a.account_id AND p.position_status ='Completed') AS total_trades,
-            (SELECT COUNT(*) FROM positions p WHERE p.account_id = a.account_id AND p.pnl > 0 AND p.position_status ='Completed') AS positive_trades,
-            (SELECT COUNT(*) FROM positions p WHERE p.account_id = a.account_id AND p.pnl <= 0 AND p.position_status ='Completed') AS negative_trades
+            (SELECT COUNT(*) FROM positions p WHERE p.account_id = a.account_id AND p.pnl_total > 0 AND p.position_status ='Completed') AS positive_trades,
+            (SELECT COUNT(*) FROM positions p WHERE p.account_id = a.account_id AND p.pnl_total <= 0 AND p.position_status ='Completed') AS negative_trades
         FROM accounts a
         LEFT JOIN transactions t ON a.account_id = t.account_id
         WHERE 1=1
